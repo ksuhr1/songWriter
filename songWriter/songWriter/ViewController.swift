2afef214e20d632d369724a2d8b90f8d392862c7
Katelyn Suhr <katy@suhr.com>
2020-03-06 19:42:28 -0800

    add empty project with core data

---
+++ songWriter/songWriter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  songWriter
+//
+//  Created by Katelyn Suhr on 3/6/20.
+//  Copyright © 2020 Katelyn Suhr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
